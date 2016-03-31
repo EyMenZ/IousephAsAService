12b2865842e37d7ec9f74a58fcae96391dc3df5e
EyMenZ <ay.zalila@gmail.com>
2016-03-30 20:02:37 -0400

    Iapi + IParser

---
+++ src/main/java/com/iouseph/api/Iapi.java
@@ -0,0 +1,60 @@
+package com.iouseph.api;
+
+import java.util.List;
+
+import com.iouseph.model.Playlist;
+import com.iouseph.model.Track;
+import com.iouseph.model.User;
+
+/**
+ * Interface pour chaque client d'API public de l'application. Ceci est pour
+ * repondre aux requis defini dans le tp1 1. outils de recherche de musique
+ * independant des systemes de streaming. 2. gestion des listes de musique. 3.
+ * reproduction des listes de musique. 4. bonus: system de parole de chanson.
+ *
+ * @author Marcial Lopez-Ferrada, Youssef Zemmahi, Aymen Zalila
+ *
+ */
+public interface Iapi {
+
+	public String retreive_token() throws Exception;
+
+	/**
+	 * methode servant a faire une recherche de chason de facon general
+	 *
+	 * @param search
+	 *            String decrivant la recherche
+	 * @return
+	 */
+	public List<Track> get_search(String search);
+
+	public List<Playlist> get_playlists(String search);
+
+	/**
+	 * methode pour recuperer une playlist
+	 *
+	 * @param playlist_id
+	 *            id de la playlist rechercher
+	 */
+	public List<Track> get_playlist(String playlist_id);
+
+	/**
+	 * methode pour recupere de l'information sur une chanson en particulier
+	 *
+	 * @param track_id
+	 *            id de la chanson rechercher
+	 */
+	public Track get_track(String track_id);
+
+	public List<Track> get_tracks();
+
+	public boolean set_playlists(List<Playlist> playlists);
+
+
+	/**
+	 * methode pour recuperer les informations personnelles de l'utilisateur
+	 *
+	 */
+	public User get_personnal_info();
+
+}
---
+++ src/main/java/com/iouseph/parsing/IParser.java
@@ -0,0 +1,61 @@
+package com.iouseph.parsing;
+
+import java.util.List;
+
+import org.json.JSONObject;
+
+import com.iouseph.model.Playlist;
+import com.iouseph.model.Track;
+import com.iouseph.model.User;
+
+public interface IParser {
+
+	/**
+	 * retourne un objet Track, il est parse a partir d'un JSONObject
+	 *
+	 * @param json	un JSONObject recupere depuis le service de streaming
+	 * @return	Track
+	 * @see IParser#tracksParse(JSONObject)
+	 */
+	public Track trackParse(JSONObject json);
+
+	/**
+	 * retourne une List<Track>, elle est parse a partir d'un JSONObject
+	 *
+	 * @param json	un JSONObject recupere depuis le service de streaming
+	 *
+	 * @return	List<Track>
+	 */
+	public List<Track> tracksParse(JSONObject json);
+
+	/**
+	 * retourne un objet Playlist, il est parse a partir d'un JSONObject
+	 *
+	 * @param json	un JSONObject recupere depuis le service de streaming
+	 * @return	Playlist
+	 * @see IParser#playlistsParse(JSONObject)
+	 */
+	public Playlist playlistParse(JSONObject json);
+
+	/**
+	 * retourne une List<Playlist>, elle est parse a partir d'un JSONObject
+	 *
+	 * @param json	un JSONObject recupere depuis le service de streaming
+	 *
+	 * @return	List<Playlist>
+	 */
+	public List<Playlist> playlistsParse(JSONObject json);
+
+	public List<Track> playlistIdParse(JSONObject json);
+
+	/**
+	 * retourne un JSONObject, il est parse a partir d'une List<Playlist>
+	 *
+	 * @param playliste	une List<Playlist> a envoyer vers le service de streaming
+	 *
+	 * @return	JSONObject
+	 */
+	public JSONObject playlistsParse(List<Playlist> playlists);
+
+	public User userParse(JSONObject json);
+}
