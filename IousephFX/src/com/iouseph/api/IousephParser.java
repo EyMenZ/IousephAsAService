c9b5047bf53c52cd710f61dab94aadd8ae53a804
Youssef ZEMMAHI <youssef.zemmahi@polymtl.ca>
2016-04-06 22:26:25 -0400

    ajout des methodes de parsing du cote du client, methode permettant de creer des instances d'utilisateur, track, playlist et autres a partir delement de format json

--- IousephFX/src/com/iouseph/api/IousephParser.java
+++ IousephFX/src/com/iouseph/api/IousephParser.java
@@ -1,7 +1,11 @@
 package com.iouseph.api;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.iouseph.model.Playlist;
@@ -10,30 +14,73 @@ import com.iouseph.model.User;
 
 public class IousephParser implements IParser {
 
+	/**
+	 * methode permetant de creer une instance track a partir d'un objet json
+	 */
 	@Override
 	public Track trackParse(JSONObject json) {
-		// TODO Auto-generated method stub
-		return null;
+		Track track = new Track();
+		track.setId(json.getString("id"));
+		track.setTitle(json.getString("title"));
+		track.setArtist(json.getString("artist"));
+		track.setAlbum(json.getString("album"));
+		track.setExternalUrl(json.getString("externalUrl"));
+		track.setImage(json.getString("image"));
+		track.setSource(json.getString("source"));
+
+		return track;
 	}
 
+	/**
+	 * methode permettant de creer une liste de tracks a partir d'un objet de
+	 * type jsonArray
+	 */
 	@Override
-	public List<Track> tracksParse(JSONObject json) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Track> tracksParse(JSONArray json) {
+		List<Track> tracks = new ArrayList<Track>();
+
+		for (int i = 0; i < json.length(); i++) {
+			Track track = this.trackParse(json.getJSONObject(i));
+			tracks.add(track);
+		}
+		return tracks;
 	}
 
+	/**
+	 * methode permettant de creer une instance playlist a partir d'un objet de
+	 * type json
+	 */
 	@Override
 	public Playlist playlistParse(JSONObject json) {
-		// TODO Auto-generated method stub
-		return null;
+		Playlist playlist = new Playlist();
+		playlist.setId(json.getString("id"));
+		playlist.setOwner(json.getString("owner"));
+		playlist.setSource(json.getString("source"));
+		playlist.setTitle(json.getString("title"));
+		playlist.setUrl(json.getString("externalUrl"));
+		playlist.setTracks(this.tracksParse(json.getJSONArray("tracks")));
+
+		return playlist;
 	}
 
+	/**
+	 * methode permettant de creer une map dont la cle correspond a l'id de la
+	 * playlist et la valeur la playlist a partir d'un objet de type jsonarray
+	 */
 	@Override
-	public List<Playlist> playlistsParse(JSONObject json) {
-		// TODO Auto-generated method stub
-		return null;
+	public Map<String, Playlist> playlistsParse(JSONArray json) {
+		Map<String, Playlist> playlists = new HashMap<String, Playlist>();
+		for (int i = 0; i < json.length(); i++) {
+			Playlist playlist = this.playlistParse(json.getJSONObject(i));
+			playlists.put(playlist.getId(), playlist);
+		}
+
+		return playlists;
 	}
 
+	/**
+	 *
+	 */
 	@Override
 	public List<Track> playlistIdParse(JSONObject json) {
 		// TODO Auto-generated method stub
@@ -46,10 +93,19 @@ public class IousephParser implements IParser {
 		return null;
 	}
 
+	/**
+	 * methode permettant de creer une instance de la classe User a partir d'un
+	 * objet de type json
+	 */
 	@Override
 	public User userParse(JSONObject json) {
-		// TODO Auto-generated method stub
-		return null;
+		User user = new User();
+		user.setId(json.getString("id"));
+		user.setPassword(json.getString("password"));
+		user.setUsername(json.getString("username"));
+		user.setPlaylists(this.playlistsParse(json.getJSONArray("playlists")));
+
+		return user;
 	}
 
 }
--- IousephFX/src/com/iouseph/model/User.java
+++ IousephFX/src/com/iouseph/model/User.java
@@ -1,5 +1,139 @@
 package com.iouseph.model;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class User {
-	//TODO cette classe sera implementee dans une prochaine version
+	private static final long serialVersionUID = 1L;
+	private UUID uid = UUID.fromString("38400000-8cf0-11bd-b23e-10b96e4ef00d");
+	private String id;
+	private String username;
+	private String password;
+	private Map<String, Playlist> playlists = new HashMap<String, Playlist>();
+
+	/**
+	 * constructeur par defaut
+	 */
+	public User() {
+		id = uid.randomUUID().toString();
+		username = new String();
+		password = new String();
+	}
+
+	/**
+	 * methode retournant l'id de l'utilisateur
+	 *
+	 * @return
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * methode permettant d'attribuer un id a l'utilisateur
+	 *
+	 * @param id
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * methode permettant de retourner une playlist selon son id
+	 *
+	 * @param playlistId
+	 * @return playlist ou null si playlist non trouve
+	 */
+	public Playlist getPlaylist(String playlistId) {
+		return playlists.get(playlistId);
+	}
+
+	/**
+	 * methode permettant d'ajouter une playlist au playlists de l'utilisateur
+	 *
+	 * @param playlist
+	 * @return true si la playlist a ete ajouter, false sinon
+	 */
+	public boolean addPlaylist(Playlist playlist) {
+		if (!playlists.containsKey(playlist)) {
+			playlists.put(playlist.getTitle(), playlist);
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * methode permettant de supprimer une playlist selon son id
+	 *
+	 * @param idPlaylist
+	 * @return true si la playlist a ete supprime, false sinonz
+	 */
+	public boolean deletePlaylist(String idPlaylist) {
+		if (!playlists.containsKey(idPlaylist)) {
+			playlists.remove(idPlaylist);
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * methode permettant de vider les playlist de l'utilisateur
+	 */
+	void emptyPlaylists() {
+		playlists.clear();
+	}
+
+	/**
+	 * accesseur au nom d'utilisateur
+	 *
+	 * @return
+	 */
+
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * setter du nom d'utilisateur
+	 *
+	 * @param username
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * getter du mot de passe
+	 *
+	 * @return
+	 */
+
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * setter du mot de passe
+	 *
+	 * @param password
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Map<String, Playlist> getPlaylists() {
+		return playlists;
+	}
+
+	public void setPlaylists(Map<String, Playlist> playlists) {
+		this.playlists = playlists;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", playlists=" + playlists
+				+ "]";
+	}
 }
